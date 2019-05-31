0314a4f58193d951c3bf5f09ba5fbf82f429e1b2
jiazimo <jiazimo@sunline.cn>
2019-05-31 18:13:48 +0800

    task

--- test.sql
+++ test.sql
@@ -9,8 +9,3 @@ select count(*) from cif_cust_base where lskhid is null or lskhid = '';
 
 -- 查询同步联社失败的sql
 select count(*) from cif_union_record where status <> '1';
-
--- 查询同步联社失败的原因sql
-select distinct(erorcd) from cif_union_record;
-
-++++++++++
